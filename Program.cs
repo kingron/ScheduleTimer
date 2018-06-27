1591c2ebc2bdf9df134fecb88552f5035a0def93
Kingron <kingron.lu@gmail.com>
2018-06-27 13:13:30 +0800

    Init commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScheduleTimer;
+using System.Timers;
+
+namespace ScheduleTimer
+{
+    class Program
+    {
+
+        static void Logs(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now + ": ST1 Trigger");
+        }
+        static void ExpiredOfDay(object sender, ElapsedEventArgs e)
+        {
+            ScheduleTimer st = (ScheduleTimer) sender;
+            Console.WriteLine(DateTime.Now + st.Tag + "ST1 Expired!");
+        }
+
+        static void St2TimerExpired(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now + ((ScheduleTimer) sender).Tag + "ST2 Expired!");
+        }
+        static void St2Log(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now + ((ScheduleTimer)sender).Tag + ": ST2 Triggered!");
+        }
+        static void Main(string[] args)
+        {
+            ScheduleTimer st = new ScheduleTimer();
+            TimeSpan Start = new TimeSpan(8, 0, 0);
+            TimeSpan End = new TimeSpan(23, 59, 59);
+            st.Init(Start, End, 30, "cmd", "/c dir");
+            st.Tag = "Schedule ST1: From 8 to 18 clock, run every 30 seconds";
+            Console.WriteLine("Lanched time: " + DateTime.Now);
+            Console.WriteLine(st.Tag);
+            st.OnScheduleTimer += Logs;
+            st.run();
+
+            ScheduleTimer st2 = new ScheduleTimer();
+            Console.WriteLine("Schedule ST2: From 12:10 to 12:12 clock, run every 20 seconds");
+            st2.Init(new TimeSpan(13, 10, 0), new TimeSpan(13, 12, 0), 20, "", "");
+            st2.OnScheduleTimer += St2Log;
+            st2.OnExpiredOfDay += St2TimerExpired;
+            st2.run();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScheduleTimer.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * The Simple Schedule Timer
+ * Simple, Easy, Fast, High Performance to run task repeated task every day.
+ * For example, run tasks every hour every day from 08:00 to 18:00
+ * Copyright Kingron, 2018
+ */
+
+using System;
+using System.Windows.Forms;
+using System.Timers;
+using System.Diagnostics;
+
+namespace ScheduleTimer
+{
+    class ScheduleTimer
+    {
+        private string Command { get; set; }
+        private string Arguments { get; set; }
+
+        private DateTime scheduledTime;
+        private System.Timers.Timer timer = null;
+
+        public TimeSpan Start { get; set; }
+        public TimeSpan End { get; set; }
+        public int Interval { get; set; }
+        public string Tag { get; set; }
+        public event ElapsedEventHandler OnScheduleTimer = null;
+        public event ElapsedEventHandler OnExpiredOfDay = null;
+
+
+        public void Init(TimeSpan start, TimeSpan end, int interval_seconds, string command, string arguments)
+        {
+            Start = start;
+            End = end;
+            Interval = interval_seconds;
+            Command = command;
+            Arguments = arguments;
+
+            scheduledTime = DateTime.Today + start;
+            timer = new System.Timers.Timer();
+            timer.Elapsed += OnTimer;
+        }
+
+        public void Stop()
+        {
+            if (timer != null) timer.Stop();
+        }
+
+        public void run()
+        {
+            if (timer == null) return;
+
+            timer.Stop();
+
+            // 如果开始时间已过，不断计算直到超过当前时间
+            while (scheduledTime < DateTime.Now)
+            {
+                scheduledTime = scheduledTime.AddSeconds(Interval);
+            }
+            timer.Interval = (double)(scheduledTime - DateTime.Now).TotalMilliseconds;
+            timer.Start();
+            scheduledTime = scheduledTime.AddSeconds(Interval);
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+
+            DateTime now = DateTime.Now;
+
+            // 判断是否当天是否到达开始时间
+            if (now < now.Date + Start)
+            {
+                scheduledTime = now.Date + Start;
+            }
+            else if (now <= now.Date + End)
+            {
+                // Time between Start & End, should do something
+                // 时间是在推送开始和结束时间范围内，需要调用事件
+                // 在这里写调用代码
+                if (Command != "" && Arguments != "")
+                {
+                    Process push = new Process();
+                    push.StartInfo.UseShellExecute = false;
+                    push.StartInfo.FileName = Command;
+                    push.StartInfo.CreateNoWindow = true;
+                    push.StartInfo.WorkingDirectory = Application.StartupPath;
+                    push.StartInfo.Arguments = Arguments;
+                    push.Start();
+                }
+                OnScheduleTimer?.Invoke(this, e);
+            }
+            else
+            {
+                // 今天过期
+                OnExpiredOfDay?.Invoke(this, e);
+            }
+            run();
+        }
+    }
+}
